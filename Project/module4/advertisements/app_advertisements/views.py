dc77f10e7eab8d9d6cd705a888bd824056cd8a40
YaroslavDemenko <140470713+YaroslavDemenko@users.noreply.github.com>
2023-08-18 13:16:09 +0900

    Add files via upload

---
+++ Project/module4/advertisements/app_advertisements/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(requests):
+    return render(requests, 'index.html')
+
+def top_sellers(requests):
+    return render(requests, 'top_sellers.html')
